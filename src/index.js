24d4dea9714fac7a91565e3698fb3dd458557e5b
Christopher Villaran <villaran.christopher@gmail.com>
2021-04-12 20:30:55 +0800

    installed axios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import axios from 'axios';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
@@ -7,9 +8,21 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import './styles/main.sass';
 import { Sample } from './components';
 
+const API_KEY = 'd5000ed59b9ce514ef986b4cd8b1044d';
+
 library.add(fab);
 library.add(far);
 library.add(fas);
+
+// Sample call 
+// Mad Max: Fury Road
+axios.get(`https://api.themoviedb.org/3/movie/76341?api_key=${ API_KEY }`)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 ReactDom.render(
   (
